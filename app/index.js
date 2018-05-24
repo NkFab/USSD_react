15cffddd9c4db7acca4121cb2a523e443f40fa5e
NkFab <fabjeezy@gmail.com>
2018-05-24 11:35:51 +0200

    Another com

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class USSD extends Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    USSD send
+                </Text>
+            </View>
+        )
+    }
+}
+export default USSD;
